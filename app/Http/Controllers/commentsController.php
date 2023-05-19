0d00b6896dfa0d3a0f9b21e612a51aee79140d39
M-Galdi <GALDI.MOHAMED@ofppt-edu.ma>
2023-05-19 09:15:32 +0100

    send2

--- app/Http/Controllers/commentsController.php
+++ app/Http/Controllers/commentsController.php
@@ -13,30 +13,7 @@ class commentsController extends Controller
     /**
      * Display a listing of the resource.
      */
-    public function index()
-    {
-        //
-    }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
-    // public function store(Request $request,  $article)
-    // {
-    //     $article = article::find($article->id);
-    //     $comment = Comment::create(['article_id' => $article->id, 'content' => $request->input('comment')]);
-    //     $article->comments()->save($comment);
-
-    //     return redirect()->route('articles.show');
-    // }
 
     public function store(Request $request, $article)
     {
@@ -48,35 +25,5 @@ class commentsController extends Controller
     }
 
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
-    {
-        //
-    }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        //
-    }
 }
